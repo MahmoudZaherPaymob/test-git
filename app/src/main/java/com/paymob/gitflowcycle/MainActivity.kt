85448252dc112aa7367256f2f7f03ea0985b21bd
MAHMOUDZAHER <mahmoudzaher.developer@gmail.com>
2023-02-26 19:10:16 +0200

    update feature branch with new comment

--- app/src/main/java/com/paymob/gitflowcycle/MainActivity.kt
+++ app/src/main/java/com/paymob/gitflowcycle/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //add comment to test feature branch
+        // new comment to the same feature
     }
 }
